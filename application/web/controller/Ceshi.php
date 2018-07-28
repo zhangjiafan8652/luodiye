d456c4a39e23d0b4e07f5202088071f6e12ecb5b
zhangjiafan <365449552@qq.com>
2018-07-28 15:23:59 +0800

    111111111111111

---
+++ application/web/controller/Ceshi.php
@@ -0,0 +1,41 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | ThinkAdmin
+// +----------------------------------------------------------------------
+// | 版权所有 2014~2017 广州楚才信息科技有限公司 [ http://www.cuci.cc ]
+// +----------------------------------------------------------------------
+// | 官方网站: http://think.ctolog.com
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// +----------------------------------------------------------------------
+// | github开源项目：https://github.com/zoujingli/ThinkAdmin
+// +----------------------------------------------------------------------
+
+namespace app\web\controller;
+
+use controller\BasicAdmin;
+use service\DataService;
+use service\NodeService;
+use service\ToolsService;
+use think\Db;
+
+/**
+ * 系统权限管理控制器
+ * Class Auth
+ * @package app\admin\controller
+ * @author Anyon <zoujingli@qq.com>
+ * @date 2017/02/15 18:13
+ */
+class Ceshi extends BasicAdmin
+{
+
+    /**
+     * @return \think\App
+     */
+    public function index()
+    {
+        return "ceshi";
+    }
+
+}
