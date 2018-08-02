5d1b98737e4b466df18731576dcfc8ee9a7266f7
zhangjiafan <365449552@qq.com>
2018-08-02 14:47:53 +0800

    111111111

--- config/log.php
+++ config/log.php
@@ -13,7 +13,7 @@
 // +----------------------------------------------------------------------
 
 return [
-    'path'        => '/www/thinkphplog/',
+    'path'        => '/www/wwwlogs/thinkphplog',
     'type'        => 'File',
     'file_size'   => 1024 * 1024 * 10,
     'apart_level' => ['emergency', 'alert', 'critical', 'error', 'sql'],
