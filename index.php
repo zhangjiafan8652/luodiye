7eba05a0313e217d7d3a0905bca9ea99f3a5b351
zhangjiafan <365449552@qq.com>
2018-10-22 14:57:59 +0800

    1111111111111111

--- index.php
+++ index.php
@@ -16,11 +16,11 @@ namespace think;
 
 
 echo "jinlaile";
-/*// 加载基础文件
+// 加载基础文件
 require __DIR__ . '/thinkphp/base.php';
 
 // think文件检查，防止TP目录计算异常
 file_exists('think') || touch('think');
 
 // 执行应用并响应
-Container::get('app', [__DIR__ . '/application/'])->run()->send();*/
+Container::get('app', [__DIR__ . '/application/'])->run()->send();
