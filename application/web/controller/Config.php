afebe96bf7f7cf7b6cb44aad1f850001dc912395
zhangjiafan <365449552@qq.com>
2018-11-22 15:06:32 +0800

    111111111111111111

--- application/web/controller/Config.php
+++ application/web/controller/Config.php
@@ -148,19 +148,40 @@ class Config extends BasicAdmin
     {
 
         // 获取表单上传文件
-        $files = request()->file('image');
-        foreach($files as $file){
-            // 移动到框架应用根目录/public/uploads/ 目录下
-            $info = $file->move(ROOT_PATH . 'static' . DS . 'uploads');
+        $file = request()->file('file1');
+
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        if($file){
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
             if($info){
                 // 成功上传后 获取上传信息
                 // 输出 jpg
                 echo $info->getExtension();
+                // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+                echo $info->getSaveName();
                 // 输出 42a79759f284b767dfcb2a0197904287.jpg
                 echo $info->getFilename();
             }else{
                 // 上传失败获取错误信息
                 echo $file->getError();
+            }
+        }
+        $file2 = request()->file('file2');
+
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        if($file2){
+            $info2 = $file2->move(ROOT_PATH . 'public' . DS . 'uploads');
+            if($info2){
+                // 成功上传后 获取上传信息
+                // 输出 jpg
+                echo $info2->getExtension();
+                // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+                echo $info2->getSaveName();
+                // 输出 42a79759f284b767dfcb2a0197904287.jpg
+                echo $info2->getFilename();
+            }else{
+                // 上传失败获取错误信息
+                echo $file2->getError();
             }
         }
     }
