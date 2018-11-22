38356bd3a28128ed957a59d2bbdcc83c30c748fb
zhangjiafan <365449552@qq.com>
2018-11-22 14:27:17 +0800

    1111111111111111

--- application/web/controller/Config.php
+++ application/web/controller/Config.php
@@ -143,6 +143,28 @@ class Config extends BasicAdmin
 
     }
 
+    //文件上传
+    public function uploadfiles()
+    {
+
+        // 获取表单上传文件
+        $files = request()->file('image');
+        foreach($files as $file){
+            // 移动到框架应用根目录/public/uploads/ 目录下
+            $info = $file->move(ROOT_PATH . 'static' . DS . 'uploads');
+            if($info){
+                // 成功上传后 获取上传信息
+                // 输出 jpg
+                echo $info->getExtension();
+                // 输出 42a79759f284b767dfcb2a0197904287.jpg
+                echo $info->getFilename();
+            }else{
+                // 上传失败获取错误信息
+                echo $file->getError();
+            }
+        }
+    }
+
 
 
 
